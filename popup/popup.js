724682702242315ca5d48c67feac51bb1fc09b1f
kestulka <kgirdiunas0@gmail.com>
2024-03-01 15:13:51 +0200

    start, reset timer

--- popup/popup.js
+++ popup/popup.js
@@ -1,30 +1,94 @@
-const tasksArr = [];
+let tasksArr = [];
+
+const startTimerBtn = document.getElementById("startTimerBtn");
+startTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.get(["isRunning"], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? "Pause Timer"
+          : "Start Timer";
+      },
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById("resetTimerBtn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+      console.log("Timer reset");
+    },
+  );
+});
 
 const addTaskBtn = document.getElementById("addTaskBtn");
 addTaskBtn.addEventListener("click", () => addTask());
 
-function addTask() {
-  const tasksCount = tasksArr.length;
-  tasksArr.push("");
+chrome.storage.sync.get(["tasksArr"], (res) => {
+  tasksArr = res.tasksArr ? res.tasksArr : [];
+  renderTasks();
+});
 
+function saveTasks() {
+  chrome.storage.sync.set({
+    tasksArr,
+  });
+}
+
+function renderTask(taskCount) {
   const taskRow = document.createElement("div");
   taskRow.classList.add("task-row");
 
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task...";
+  text.value = tasksArr[taskCount];
   text.addEventListener("change", () => {
-    tasksArr[tasksCount] = text.value;
+    tasksArr[taskCount] = text.value;
+    saveTasks();
     console.log(tasksArr);
   });
 
   const deleteBtn = document.createElement("input");
   deleteBtn.type = "button";
   deleteBtn.value = "X";
+  deleteBtn.addEventListener("click", () => {
+    deleteTask(taskCount);
+  });
 
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
 
   const taskContainer = document.getElementById("taskContainer");
   taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  const taskCount = tasksArr.length;
+  tasksArr.push("");
+  renderTask(taskCount);
+  saveTasks();
+}
+
+function deleteTask(taskCount) {
+  tasksArr.splice(taskCount, 1);
+  renderTasks();
+  saveTasks();
+}
+
+function renderTasks() {
+  const taskContainer = document.getElementById("taskContainer");
+  taskContainer.textContent = "";
+  tasksArr.forEach((taskText, taskCount) => {
+    renderTask(taskCount);
+  });
 }
